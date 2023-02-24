926f0faed55f9798d64fb694aa6a5d5698b0b39d
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-02-24 18:21:55 +0000

    add function for show score

--- scripts/game.js
+++ scripts/game.js
@@ -49,3 +49,20 @@ function lightsOn(circ) {
         document.getElementById(circ).classList.remove("light");
     }, 400);
 }
+function playerTurn() {
+    let i = game.playerMoves.length - 1;
+    if (game.currentGame[i] === game.playerMoves[i]) {
+        if (game.currentGame.length === game.playerMoves.length) {
+            game.score++;
+            showScore();
+            addTurn();
+        }
+    } else {
+        alert("Wrong move!");
+        newGame();
+    }
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}

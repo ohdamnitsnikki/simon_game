285e62175158386eeb042cecfa26603494626977
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-02-24 18:19:19 +0000

    add functions for new game

--- scripts/game.js
+++ scripts/game.js
@@ -5,3 +5,23 @@ let game = {
     turnNumber: 0,
     choices: ["button1", "button2", "button3", "button4"]
 };
+
+function newGame() {
+    game.currentGame = [];
+    game.playerMoves = [];
+    game.score = 0;
+
+    for (let circle of document.getElementsByClassName("circle")) {
+        if (circle.getAttribute("data-listener") !== "true") {
+            circle.addEventListener("click", (e) => {
+                let move = e.target.getAttribute("id");
+                game.playerMoves.push(move);
+                lightsOn(move);
+                playerTurn();
+            });
+            circle.setAttribute("data-listener", "true");
+        }
+    }
+    showScore();
+    addTurn();
+}
